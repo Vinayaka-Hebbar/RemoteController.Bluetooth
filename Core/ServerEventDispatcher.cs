b95c716a519d9c4c6214a100313391529457e901
vinayakahebbar <vinayakahebbar@hotmail.com>
2020-11-22 12:32:44 +0530

    added ioException

--- Core/ServerEventDispatcher.cs
+++ Core/ServerEventDispatcher.cs
@@ -53,6 +53,10 @@ namespace RemoteController.Core
             {
                 // socket closed
             }
+            catch (System.IO.IOException)
+            {
+                // invalid write
+            }
         }
 
         public void Process(IMessage message)
