1273b5cf2f4af934bb72cd95b7864c9df56d9e4b
Elpis-PC <Elpis-PC@DESKTOP-DONO3I1>
2020-11-22 15:36:32 +0530

    updated

--- Core/RemoteClient.cs
+++ Core/RemoteClient.cs
@@ -53,7 +53,7 @@ namespace RemoteController.Core
                     screenConfiguration.AddScreenLeft(last, screen.X, screen.Y, screen.Width, screen.Height, screen.Client);
                 }
             }
-            var s = state.ScreenConfiguration.GetFurthestLeft();
+            var s = state.ScreenConfiguration.GetFurthestRight();
             state.VirtualX = s.X;
             state.VirtualY = s.Y;
             _hook.Hook.SetMousePos(state.LastPositionX, state.LastPositionY);
